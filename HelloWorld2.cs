0f1687a688dec57fef7f0cea138e9f7aeaecfcb2
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2022-09-27 09:16:30 +0200

    HelloWorld op api

---
+++ HelloWorld2.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MCT.Function
+{
+    public static class HelloWorld2
+    {
+        [FunctionName("HelloWorld2")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var name = "HelloWorld";
+
+            
+
+            return new OkObjectResult(name);
+        }
+    }
+}
